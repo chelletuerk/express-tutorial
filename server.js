17701560738529f184e3ba930610cfcac2a985bc
chelle tuerk <michelletuerk@gmail.com>
2017-03-10 08:27:17 -0700

    home page requests

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
+
+var bodyParser = require('body-parser');
+var multer  = require('multer');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+var upload = multer({dest: 'C:/tmp/'});
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
+app.post('/file_upload', upload.single('file'), function (req, res) {
+   console.log(req.files.file.name);
+   console.log(req.files.file.path);
+   console.log(req.files.file.type);
+   var file = __dirname + "/" + req.files.file.name;
+
+   fs.readFile( req.files.file.path, function (err, data) {
+      fs.writeFile(file, data, function (err) {
+         if( err ){
+            console.log( err );
+            }else{
+               response = {
+                  message:'File uploaded successfully',
+                  filename:req.files.file.name
+               };
+            }
+         console.log( response );
+         res.end( JSON.stringify( response ) );
+      });
+   });
 })
 
 var server = app.listen(8081, function () {
@@ -12,5 +39,4 @@ var server = app.listen(8081, function () {
    var port = server.address().port
 
    console.log("Example app listening at http://%s:%s", host, port)
-
 })
